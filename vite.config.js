fd7bb817ae80031f4737f51f01f1e306f25606c1
Chandan8018 <chandankumarsahoo19@gmail.com>
2024-07-14 03:27:07 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "Fyle_Assignment",
+  build: {
+    outDir: "dist",
+  },
+});
